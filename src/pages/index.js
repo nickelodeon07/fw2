76b631d7daa355c6edbe6c2ae21562bcaf16c852
nickelodeon07 <160284236+nickelodeon07@users.noreply.github.com>
2024-09-30 15:44:29 +0000

    AULA 30-09

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,23 @@
 import Footer from "./component/footer";
 import Navbar from "./component/navbar";
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function Index() {
-    return
-    <>
-        <Navbar/>
-        <h1>OLa mundo, eu estou vivo.</h1>
-        <Footer/>
+    return <>
+        <Navbar />
+        <Container>
+            <Row>
+                <Col>1 of 2</Col>
+                <Col>2 of 2</Col>
+            </Row>
+            <Row>
+                <Col>1 of 3</Col>
+                <Col>2 of 3</Col>
+                <Col>3 of 3</Col>
+            </Row>
+        </Container>
+        <Footer />
     </>
 
 }
