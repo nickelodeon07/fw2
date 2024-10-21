54a56ade91d6a9a6e0ae753359c5972e2e5855f0
nickelodeon07 <160284236+nickelodeon07@users.noreply.github.com>
2024-10-21 12:26:30 -0300

    Recuperado o código da aula 21/10

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,21 @@
 import Footer from "./component/footer";
-import Navbar from "./component/navbar";
 import {Container, Row, Col} from 'react-bootstrap';
-function Index() {
+import Menu from "./component/navbar";
+
+export default function Index() {
     return <>
-        <Navbar />
-        <Container>
+        <Menu />
+        <Container className="px-4 my-3">
             <Row>
-                <Col className="bg-primary-subtle">1 of 2</Col>
-                <Col className="bg-secondary-subtle">2 of 2</Col>
+                <Col className="bg-primary">1 of 2</Col>
+                <Col className="bg-success">2 of 2</Col>
             </Row>
             <Row>
-                <Col className="bg-success-subtle">1 of 3</Col>
-                <Col className="bg-warning-subtle">2 of 3</Col>
-                <Col className="bg-body-secondary">3 of 3</Col>
+                <Col className='bg-warning'>2 of 3</Col>
+                <Col className='bg-info-subtle'>1 of 3</Col>
+                <Col className='grad'>3 of 3</Col>
             </Row>
         </Container>
         <Footer />
-    </>
-
+    </>;
 }
-export default Index;
--- src/pages/noticias/[id].js
+++ src/pages/noticias/[id].js
@@ -1,6 +1,6 @@
-import { useRouter } from "next/router";
-export default function Page(){
-    const router = userRouter()
+import { useRouter } from "next/router"
+export default function Page() {
+    const router = useRouter()
     return <>
         <p>Post: {router.query.id}</p>
     </>
